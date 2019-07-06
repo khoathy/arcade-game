6acc0bcbc0c701b2ce9f79cc5a3203b150e15a38
KhoaThy <ms.khoathy@gmail.com>
2019-07-05 23:30:55 -0700

    feat: show enemies crossing the screen

--- js/app.js
+++ js/app.js
@@ -5,18 +5,25 @@ class Enemy {
         this.x = x;
         this.y = y;
         this.sprite = 'images/enemy-bug.png';
+        this.width = 96;
+        this.height = 65;
+        this.speed = 250;
     }
 
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        console.log('update enemy');
+        if (this.x - this.width > ctx.canvas.width) {
+            this.x = -150 * Math.floor(Math.random() * 5) + 1 ;
+        } else {
+            this.x += (Math.floor(Math.random()) + 1) * this.speed * dt;
+        }
     }
 
     // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        console.log('render enemy');
+        // console.log(this);
     }
 };
 
@@ -40,7 +47,7 @@ class Player {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
     }
 
-    // a handleInput() method
+    // Move player around but only inside canvas
     handleInput(move) {
         const horizontal = 100;
         const vertical = 83;
@@ -58,10 +65,12 @@ class Player {
 
 
 
-const enemy = new Enemy(100,200);
 const player = new Player(5,380,'images/char-boy.png');
 
-const allEnemies = [];
+const enemyY = [55, 140, 230];
+const allEnemies = enemyY.map((y, index) => {
+    return new Enemy(-150 * (index + 1) ,y);
+});
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
